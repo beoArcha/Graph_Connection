59e14f870f0cecf702b354e20a63ec942959e302
Marcin Stefanski <marcinstefanski@outlook.com>
2020-03-07 08:36:54 +0100

    setup upgrade

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Marcin Stefański',
     author_email='',
     description='',
-    install_requires=['docutils>=0.3', 'networkx=2.1', 'scipy>=1.0', 'pandas=0.23', 'numpy>=1.13.3',
+    install_requires=['docutils>=0.3', 'networkx=2.1', 'scipy>=1.0', 'pandas=0.25', 'numpy>=1.13.3',
                       'scikit-learn>=0.19', "neo4j"]
 )
 
